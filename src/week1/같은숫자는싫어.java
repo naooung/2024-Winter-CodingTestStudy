ef8381ecc1c567d334a8600299f36bc735d56e5e
naooung <1030n@naver.com>
2025-01-04 17:18:18 +0900

    week1

--- src/week1/같은숫자는싫어.java
+++ src/week1/같은숫자는싫어.java
@@ -1,4 +1,26 @@
 package week1;
 
+import java.util.ArrayDeque;
+
 public class 같은숫자는싫어 {
+    public int[] solution(int []arr) {
+        ArrayDeque<Integer> q = new ArrayDeque<>();
+
+        // 이전 숫자와 다르면 큐에 push
+        q.add(arr[0]);
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i - 1] != arr[i]) {
+                q.addLast(arr[i]);
+            }
+        }
+
+        // 큐에서 배열로 변환
+        int size = q.size();
+        int[] result = new int[size];
+        for (int i = 0; i < size; i++) {
+            result[i] = q.pollFirst();
+        }
+
+        return result;
+    }
 }
--- src/week1/햄버거만들기.java
+++ src/week1/햄버거만들기.java
@@ -3,24 +3,4 @@ package week1;
 import java.util.ArrayDeque;
 
 public class 햄버거만들기 {
-    public int[] solution(int []arr) {
-        ArrayDeque<Integer> q = new ArrayDeque<>();
-
-        // 이전 숫자와 다르면 큐에 push
-        q.add(arr[0]);
-        for (int i = 1; i < arr.length; i++) {
-            if (arr[i - 1] != arr[i]) {
-                q.addLast(arr[i]);
-            }
-        }
-
-        // 큐에서 배열로 변환
-        int size = q.size();
-        int[] result = new int[size];
-        for (int i = 0; i < size; i++) {
-            result[i] = q.pollFirst();
-        }
-
-        return result;
-    }
 }
